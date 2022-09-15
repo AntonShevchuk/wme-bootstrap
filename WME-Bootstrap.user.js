c51c5dd6ac55d72a5d377ad33a439577c99ef6d8
Anton Shevchuk <antonshevchuk@gmail.com>
2022-09-15 14:50:02 +0300

    Updated matching rules

--- WME-Bootstrap.user.js
+++ WME-Bootstrap.user.js
@@ -6,12 +6,9 @@
 // @author       Anton Shevchuk
 // @namespace    https://greasyfork.org/users/227648-anton-shevchuk
 // @supportURL   https://github.com/AntonShevchuk/wme-bootstrap/issues
-// @match        https://www.waze.com/editor*
-// @match        https://www.waze.com/*/editor*
-// @match        https://beta.waze.com/editor*
-// @match        https://beta.waze.com/*/editor*
-// @exclude      https://www.waze.com/user/editor*
-// @exclude      https://beta.waze.com/user/editor*
+// @match        https://*.waze.com/editor*
+// @match        https://*.waze.com/*/editor*
+// @exclude      https://*.waze.com/user/editor*
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://anton.shevchuk.name&size=64
 // @grant        none
 // ==/UserScript==
